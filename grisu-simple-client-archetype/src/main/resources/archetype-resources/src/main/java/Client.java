7d47c653b4ccf490b359115664d1bcec637391fc
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-04-20 14:39:43 +1200

    Fixed version numbers, moved project out of grisu-clients.

--- grisu-simple-client-archetype/src/main/resources/archetype-resources/src/main/java/Client.java
+++ grisu-simple-client-archetype/src/main/resources/archetype-resources/src/main/java/Client.java
@@ -1,5 +1,7 @@
 package ${groupId};
 
+import grisu.model.FileManager;
+
 import org.vpac.grisu.control.ServiceInterface;
 import org.vpac.grisu.control.exceptions.JobPropertiesException;
 import org.vpac.grisu.control.exceptions.JobSubmissionException;
@@ -18,25 +20,28 @@ public class Client {
 		try {
 			si = LoginManager.loginCommandline("BeSTGRID");
 		} catch (Exception e) {
-			System.err.println("Could not login: "+e.getLocalizedMessage());
+			System.err.println("Could not login: " + e.getLocalizedMessage());
 			System.exit(1);
 		}
 
 		System.out.println("Creating job...");
 		JobObject job = new JobObject(si);
 		job.setApplication("UnixCommands");
-		job.setCommandline("echo \"hello grid\"");
+		String filename = FileManager.getFilename(args[0]);
+		job.setCommandline("cat " + filename);
+		job.addInputFileUrl(args[0]);
 		job.setWalltimeInSeconds(60);
 
-		job.setTimestampJobname("MyFirstJob");
+		job.setTimestampJobname("cat_job");
 
-		System.out.println("Set jobname to be: "+job.getJobname());
+		System.out.println("Set jobname to be: " + job.getJobname());
 
 		try {
 			System.out.println("Creating job on backend...");
 			job.createJob("/ARCS/BeSTGRID");
 		} catch (JobPropertiesException e) {
-			System.err.println("Could not create job: "+e.getLocalizedMessage());
+			System.err.println("Could not create job: "
+					+ e.getLocalizedMessage());
 			System.exit(1);
 		}
 
@@ -44,27 +49,33 @@ public class Client {
 			System.out.println("Submitting job to the grid...");
 			job.submitJob();
 		} catch (JobSubmissionException e) {
-			System.err.println("Could not submit job: "+e.getLocalizedMessage());
+			System.err.println("Could not submit job: "
+					+ e.getLocalizedMessage());
 			System.exit(1);
 		} catch (InterruptedException e) {
-			System.err.println("Jobsubmission interrupted: "+e.getLocalizedMessage());
+			System.err.println("Jobsubmission interrupted: "
+					+ e.getLocalizedMessage());
 			System.exit(1);
 		}
 
 		System.out.println("Job submission finished.");
-		System.out.println("Job submitted to: "+job.getJobProperty(Constants.SUBMISSION_SITE_KEY));
+		System.out.println("Job submitted to: "
+				+ job.getJobProperty(Constants.SUBMISSION_SITE_KEY));
 
 		System.out.println("Waiting for job to finish...");
 
-		// for a realy workflow, don't check every 5 seconds since that would put too much load on the backend/gateways
+		// for a realy workflow, don't check every 5 seconds since that would
+		// put too much load on the backend/gateways
 		job.waitForJobToFinish(5);
 
-		System.out.println("Job finished with status: "+job.getStatusString(false));
+		System.out.println("Job finished with status: "
+				+ job.getStatusString(false));
 
-		System.out.println("Stdout: "+job.getStdOutContent());
-		System.out.println("Stderr: " +job.getStdErrContent());
-		
-		// it's good practise to shutdown the jvm properly. There might be some executors running in the background
+		System.out.println("Stdout: " + job.getStdOutContent());
+		System.out.println("Stderr: " + job.getStdErrContent());
+
+		// it's good practise to shutdown the jvm properly. There might be some
+		// executors running in the background
 		// and they need to know when to shutdown.
 		// Otherwise, your application might not exit.
 		System.exit(0);
