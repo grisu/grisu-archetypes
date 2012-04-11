5dea6c7f6f89dc08b2523f23dee9c08734b78d83
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-04-11 14:29:19 +1200

    Slightly improved groovy template.

--- grisu-groovy-client-archetype/src/main/resources/archetype-resources/src/main/groovy/GroovyClient.groovy
+++ grisu-groovy-client-archetype/src/main/resources/archetype-resources/src/main/groovy/GroovyClient.groovy
@@ -3,33 +3,98 @@ package ${groupId}
 import grisu.frontend.control.login.LoginManager
 import grisu.frontend.model.job.JobObject
 import grisu.jcommons.constants.Constants
+import grisu.model.GrisuRegistryManager
 
-LoginManager.initGrisuClient('$artifactId')
+class GroovyClient {
 
-def si = LoginManager.loginCommandline('testbed')
+	static void main(String[] args) {
+		LoginManager.initGrisuClient('$artifactId')
 
-def job = new JobObject(si)
 
-job.setApplication(Constants.GENERIC_APPLICATION_NAME)
+		def cli = new CliBuilder(usage:'$artifactId -g group [options] commandline')
+		cli.b(longOpt:'backend', args:1, 'The backend (alias or url) to connect to (default: testbed)')
+		cli.g(longOpt:'group', args:1, 'The group used to submit the job (required)')
+		cli.f(longOpt: 'files', args:1, 'Comma seperated list of urls or paths to input files for this job')
+		cli.j(longOpt: 'jobname', args:1, 'Jobname')
+		
+		def options = cli.parse(args)
+		if (! options ) {
+			cli.usage()
+			System.exit(1)
+		}
+		// make sure a group is set
+		if (! options.g) {
+			println 'No group specified.'
+			cli.usage()
+			System.exit(1)
+		}
+		// make sure arguments exist
+		if (! options.arguments() ) {
+			println 'No commandline specified.'
+			cli.usage()
+			System.exit(1)
+		}
 
-// this is just to show how to access Java from within this script
-def echoString = ${groupId}.ExampleJavaClass.echoString
 
-job.setCommandline('echo ' + echoString)
+		def commandline = options.arguments().join(' ')
 
-job.setTimestampJobname('groovy_job')
+		def backend = options.b
+		if ( ! backend ) {
+			backend = 'testbed'
+		}
+		
+		def jobname = options.j
+		if ( ! jobname ) {
+			jobname = options.arguments()[0]+'_job'
+		}
 
-println 'Creating job...'
-job.createJob('/none')
-println 'Submitting job...'
-job.submitJob()
 
-println 'Waiting for job...'
-job.waitForJobToFinish(5)
+		def si = LoginManager.loginCommandline(backend)
+		
+		// check whether the specified group exists
+		if (! si.getFqans().asSortedSet().contains(options.g) ) {
+			println 'You are not a member of group "'+options.g+'".'
+			System.exit(1)
+		}
 
-println 'Job finished with status: ' + job.getStatusString(false)
+		def fm = GrisuRegistryManager.getDefault(si).getFileManager()
+		// checking whether all input files exist
+		def files = []
+		if ( options.f ) {
+			files = options.f.split(',')
+			for ( def file : files ) {
+				if ( ! fm.fileExists(file) ) {
+					println 'Input file "'+file+'" does not exist'
+					System.exit(1)
+				}
+			}
+		}
 
-println 'Stdout: ' + job.getStdOutContent()
-println 'Stderr: ' + job.getStdErrContent()
+		def job = new JobObject(si)
 
-System.exit(0)
+		job.setApplication(Constants.GENERIC_APPLICATION_NAME)
+
+		// this is just to show how to access Java from within this script
+		// doesn't make any sense in context of this script
+		def echoString = ${groupId}.ExampleJavaClass.echoString
+
+		job.setCommandline(commandline)
+
+		job.setTimestampJobname(jobname)
+
+		println 'Creating job "'+job.getJobname()+'"...'
+		job.createJob(options.g)
+		println 'Submitting job...'
+		job.submitJob()
+
+		println 'Waiting for job...'
+		job.waitForJobToFinish(5)
+
+		println 'Job finished with status: ' + job.getStatusString(false)
+
+		println 'Stdout: ' + job.getStdOutContent()
+		println 'Stderr: ' + job.getStdErrContent()
+
+		System.exit(0)
+	}
+}
